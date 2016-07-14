71021d8d9532a2cb69a8c70ecce6476fb0333b48
Robin Hopkins <robin.hopkins@cgi.com>
2016-07-14 15:27:34 +0100

    initial

---
+++ src/cent6app1/Cent6app1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cent6app1;
+
+/**
+ *
+ * @author robin
+ */
+public class Cent6app1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Cent6app1");
+    }
+    
+}
